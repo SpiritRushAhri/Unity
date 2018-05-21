b91e910454e371737889865d4397fc0f8f6f66d5
Spirit <spiritrushahri@live.com>
2018-05-20 21:26:26 -0700

    Started the Rise rocket project!
    Built the basic rocket and started on reading in user input

---
+++ Rise/Assets/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		UserInput();
+	}
+   // Process the user input
+   private void UserInput () {
+      // Space is ship boosters
+      if (Input.GetKey(KeyCode.Space)) {
+         print("Boosting!");
+      }
+      // A is rotate left
+      if (Input.GetKey(KeyCode.A)) {
+         print("Rotate Left");
+      }
+      // D is rotate right
+      else if (Input.GetKey(KeyCode.D)) {
+         print("Rotate Right");
+      }
+   }
+}
